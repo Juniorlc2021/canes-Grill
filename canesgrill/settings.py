45ca0872af3d39a4322d0c00c00da0f98e371364
Juniorlc2021 <lcdancemusic@gmail.com>
2023-06-23 22:21:55 -0300

    continua projeto

--- canesgrill/settings.py
+++ canesgrill/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'churras',
+    'churras','usuarios','pessoas',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pessoas',
+  
 ]
 
 MIDDLEWARE = [
